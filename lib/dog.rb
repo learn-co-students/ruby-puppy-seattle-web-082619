868ea5e00fd1c2b76bddb5d3f0851edfe0ca3665
renburnett <renardburnett@gmail.com>
2019-08-29 11:30:51 -0700

    implemented Dog class

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,27 @@
-# Add your code here
+class Dog
+    @@all  = []
+    attr_accessor :name
+
+    def initialize(name)
+        @name = name
+        save()
+    end
+    
+    def Dog.all()
+        return @@all
+    end
+    
+    def Dog.clear_all()
+        @@all.clear
+    end
+
+    def Dog.print_all()
+        @@all.each do |ele|
+            puts ele.name
+        end
+    end
+
+    def save()
+        @@all << self
+    end
+end
