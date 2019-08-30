b4509be22a165cd649874a65cdf0224e5e56b0b7
github scarlettutopia <github scarlettutopia@aol.com>
2019-08-30 13:41:40 -0700

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,37 @@
-# Add your code here
+require 'pry'
+class Dog
+    attr_reader :name
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        # @@all << self
+        self.save
+        # binding.pry
+    end
+
+    # def self
+    #     @@all
+    # end
+
+    def self.all
+        @@all
+    end
+
+    def self.clear_all
+        @@all.clear
+    end
+
+    def self.print_all
+        self.all.each {|dog| puts "#{dog.name}"}
+    end
+
+    def save
+        @@all << self
+    end
+
+end
+# Pry.start
+
+# 0
