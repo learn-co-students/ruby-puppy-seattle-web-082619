ecccd536d1ae81802f2c09d04ea86aee73779096
Brian Ly <brian.kyang.ly@gmail.com>
2019-08-30 12:07:05 -0700

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,37 @@
 # Add your code here
+require 'pry'
+
+class Dog
+    attr_reader :name
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+
+        self.save #instead of manually pushing each instance, we can call the
+        #class method, .save, which we have defined later in the development process
+        
+    end
+
+    def self.all
+        @@all 
+    end 
+
+    def self.clear_all
+        @@all = []
+    end
+
+    def self.print_all
+        print_all = ""
+        @@all.each do |dog|
+            print_all = print_all + "#{dog.name}\n"
+            
+        end
+        puts print_all #the test here says that the output goes to stdout
+    end
+   
+    def save
+        @@all << self
+    end
+end
